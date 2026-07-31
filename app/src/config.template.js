d20f6b410aeb30d9b37cd3033b4d7306287a7e3e
feel-back-offline <igoshin.e.a@gmail.com>
2026-07-31 17:54:35 +0300

    Update copyright text across multiple files for consistency. Changed "Все права защищены" to "Все права точно защищены" in config.template.js, Brand.js, and various HTML views to ensure uniformity in copyright statements.

--- app/src/config.template.js
+++ app/src/config.template.js
@@ -1292,7 +1292,7 @@ module.exports = {
                 html: `
                     <hr />
                     <span>
-                        &copy; ${new Date().getFullYear()} ${process.env.COPYRIGHT_TEXT || 'OPTRF. Все права защищены.'}
+                        &copy; ${new Date().getFullYear()} ${process.env.COPYRIGHT_TEXT || 'OPTRF. Все права точно защищены'}
                     </span>
                     <hr />
                     `,
